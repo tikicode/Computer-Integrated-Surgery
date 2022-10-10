ffb739fa1ed31a28da1544ba20f77996fa9d8325
Ilana Chalom <ichalom1@jhu.edu>
2022-10-10 02:16:18 -0400

    created frame class and methods

--- include/frame.hpp
+++ include/frame.hpp
@@ -4,19 +4,40 @@
 #include<Eigen/Dense>
 
 using namespace Eigen;
+class Frame {
+    public:
+    Matrix3f R;
+    Vector3d p;
 
-Frame()
+    Frame(Matrix3f R, Vector3d p) {
+        R = R;
+        p = p;
+    }
 
-Frame composeFrame(Frame other) {
+    Frame composeFrame(Frame other) {
+        Vector3d vec = this->R*other.p;
+        vec+=this->p;
+        Matrix3f mat = this->R*other.R;
+        return Frame(mat, vec);
+    }
 
+    Vector3d composeVec(Vector3d v){
+        Vector3d vec = this->R*v;
+        vec+=this->p;
+        return vec;
+    }
+
+    Frame invert() {
+        Matrix3f mat;
+        if(this->R.determinant() != 0) {
+            mat = this->R.inverse();
+        } 
+        // else do smtg to matrix to make it invertible
+        Vector3d vec = mat*this->p;
+        vec*= -1;
+        return Frame(mat, vec);
+    }
 }
 
-Vector3d composeVec(Vector3d){
-
-}
-
-Frame invert() {
-    
-}
 
 #endif
