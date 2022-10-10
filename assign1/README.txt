97206e6244d3c3768e76dec69263144861809110
tikicode <anukala1@jhu.edu>
2022-10-10 01:55:26 -0400

    could be wrong

--- assign1/README.txt
+++
@@ -1,4 +0,0 @@
-CIS 1 Code Library
-Authors: Arijit Nukala, Ilana Chalom 
-
-link to cite the linalg package: https://github.com/sgorsten/linalg#matrix-algebra
---
+++ include/frame.hpp
@@ -0,0 +1,22 @@
+#ifndef _FRAME_HPP_
+#define _FRAME_HPP_
+
+#include<Eigen/Dense>
+
+using namespace Eigen;
+
+Frame()
+
+Frame composeFrame(Frame other) {
+
+}
+
+Vector3d composeVec(Vector3d){
+
+}
+
+Frame invert() {
+    
+}
+
+#endif
