2c8ab4218c5d846e9810d9194e571b95f92be8b1
Ilana Chalom <ichalom1@jhu.edu>
2022-10-10 14:48:51 -0400

    started working on registration algorithm

---
+++ include/pointSet.hpp
@@ -0,0 +1,35 @@
+#ifndef _FRAME_HPP_
+#define _FRAME_HPP_
+
+#include<Eigen/Dense>
+#include "frame.hpp"
+
+using namespace Eigen;
+class PointSet() {
+
+public:
+Matrix3f points;
+
+PointSet(Matrix3f p) {
+    points = p;
+}
+
+Frame registration(PointSet other) {
+    //callibrate center of gravity somehow?
+    //do smtg w the means
+    PointSet((*this)*(other.transpose())) a;
+    JacobiSVD<Matrix3f, ComputeThinU | ComputeThinV> svd(a);
+    PointSet(svd.matrixU()) U;
+    U = U.transpose();
+    Matrix3f s = svd.singularValues().asDiagonal();
+    Matrix3f R = S*(svd.mattrixV().transform);
+    R = U*R;
+    //this isn't entirely correct
+    //have to account for error
+}
+
+
+
+}
+
+#endif
