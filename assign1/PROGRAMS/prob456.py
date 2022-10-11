66d747b65154b7e04ebb28bc3d1a016ac986f96f
Ilana Chalom <ichalom1@jhu.edu>
2022-10-11 16:34:13 -0400

    problem 4 done mostly

--- assign1/PROGRAMS/prob456.py
+++ assign1/PROGRAMS/prob456.py
@@ -5,18 +5,28 @@ import PointSet
 
 def probFour(calbody, calReading):
     # do something to read the files
-    calbod = open(calbody, "r")
-    calread = open(calReading, "r")
+    # calbod = open(calbody, "r")
+    # calread = open(calReading, "r")
+    # read file method
 
-    
+
     dPoints = calbody d points
     aPoints = calbody a points
     cPoints = calbody c points
     Frames = calReading frame
-
+    cExp = []
     for frame in Frames :
         DPoints = calReading at given frame index d points
         APoints = calReading at given frame index a points
         FD = registration(dPoints, DPoints)
         FA = registration(aPoints, APoints)
-        Ci = 
+        Ci = FA.composeVec(cPoints)
+        Ci = (FD.invert()).composeFrame(Ci)
+        cExp.append(Ci)
+
+    return cExp
+
+
+
+
+        
