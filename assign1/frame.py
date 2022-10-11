a318eecfa41232178a7146c758df319277e33fd5
tikicode <anukala1@jhu.edu>
2022-10-11 16:30:10 -0400

    added environment and requirements files

--- assign1/frame.py
+++ assign1/frame.py
@@ -1,22 +1,21 @@
-import numpy
+import numpy as np
 class Frame:
-
     def __init__(self, R, p):
         self.R = R
         self.p = p
 
     def composeFrame(self, otherFrame):
-        vec = numpy.dot(self.R, otherFrame.p)
+        vec = np.dot(self.R, otherFrame.p)
         vec += self.p
-        mat = numpy.dot(self.R, otheFrame.R)
+        mat = np.dot(self.R, otherFrame.R)
         return Frame(mat, vec)
 
     def composeVec(self, v):
-        vec = numpy.dot(self.R,v)
+        vec = np.dot(self.R,v)
         vec+=self.p
         return vec
     
     def invert(self):
-        newR = numpy.linalg.inv(self.R)
-        newP = numpy.dot(-1*newR, self.p)
+        newR = np.linalg.inv(self.R)
+        newP = np.dot(-1*newR, self.p)
         return Frame(newR, newP)
--- assign1/pointSet.py
+++ assign1/pointSet.py
@@ -1,5 +1,5 @@
 import numpy as np
-import Frame
+import frame
 
 class PointSet:
 
