ae13df8020574a1c3639d5bb83378c97c2e8968a
tikicode <anukala1@jhu.edu>
2022-10-12 10:24:24 -0400

    error fixes for pointSet

--- cis/point_set.py
+++ cis/point_set.py
@@ -13,6 +13,8 @@ class PointSet:
     parameters: two point sets
     return: frame for the transformation
     '''
+
+
 def registration(A, B):
     centeredA = A - np.mean(A.points, axis=1)
     centeredB = B - np.mean(B.points, axis=1)
@@ -27,13 +29,16 @@ def registration(A, B):
     R = np.dot(vt, u)
 
     # check to make sure not reflection
-    if np.linalg.det( R ) < 0:
-         R[:, 3] *= -1
+    if np.linalg.det(R) < 0:
+        R[:, 3] *= -1
 
     p = centeredB - np.dot(R, centeredA)
     return fr.Frame(R, p)
 
+
 '''Methods to read each type of file'''
+
+
 def getDataCalBody(fName):
     headers = pd.read_csv(fName, header=None, names=["Nd", "Na", "Nc", np.nan], nrows=1)
     # number of each
@@ -45,16 +50,17 @@ def getDataCalBody(fName):
             PointSet(np.array(text[["xi", "yi", "zi"]][1 + ND: 1 + ND + NA])),
             PointSet(np.array(text[["xi", "yi", "zi"]][1 + ND + NA:])))
 
+
 def getDataCalReading(fName):
-    headers = pd.read_csv(fName, header=None, names=["Nd", "Na", "Nc", "Nf", np.nan]nrows=1)
-    #number of each
+    headers = pd.read_csv(fName, header=None, names=["Nd", "Na", "Nc", "Nf", np.nan], nrows=1)
+    # number of each
     ND = int(headers["Nd"][0])
     NA = int(headers["Na"][0])
     NC = int(headers["Nc"][0])
     NFrame = int(headers["Nf"][0])
     text = pd.read_csv(fName, header=None, names=["xi", "yi", "zi"], skiprows=1)
 
-    D, A, C = []
+    D, A, C = [], [], []
 
     for frame in range(NFrame):
         ind = frame * (ND + NA + NC)
@@ -63,9 +69,10 @@ def getDataCalReading(fName):
         C.append(PointSet(np.array(text[["xi", "yi", "zi"]][1 + ND + NA + NC + ind:])))
     return D, A, C
 
+
 def getDataEMPivot(fName):
-    headers = pd.read_csv(fName, header=None, names=["Ng", "Nf", np.nan]nrows=1)
-    #number of each
+    headers = pd.read_csv(fName, header=None, names=["Ng", "Nf", np.nan], nrows=1)
+    # number of each
     NG = int(headers["Ng"][0])
     NFrame = int(headers["Nf"][0])
     text = pd.read_csv(fName, header=None, names=["xi", "yi", "zi"], skiprows=1)
@@ -73,23 +80,23 @@ def getDataEMPivot(fName):
     G = []
 
     for frame in range(NFrame):
-        ind = frame*NG
-        G.append(PointSet(np.array(text[["xi", "yi", "zi"]][1+ind:1+NG+ind])))
+        ind = frame * NG
+        G.append(PointSet(np.array(text[["xi", "yi", "zi"]][1 + ind:1 + NG + ind])))
     return G
 
+
 def getDataOptPivot(fName):
-    headers = pd.read_csv(fName, header=None, names=["Nd", "Nh", "Nf", np.nan]nrows=1)
-    #number of each
+    headers = pd.read_csv(fName, header=None, names=["Nd", "Nh", "Nf", np.nan], nrows=1)
+    # number of each
     ND = int(headers["Nd"][0])
     NH = int(headers["Nh"][0])
     NFrame = int(headers["Nf"][0])
     text = pd.read_csv(fName, header=None, names=["xi", "yi", "zi"], skiprows=1)
 
-    D, H = [],[]
+    D, H = [], []
 
     for frame in range(NFrame):
-        ind = frame*(ND+NH)
-        D.append(PointSet(np.array(text[["xi", "yi", "zi"]][1+ind:1+ND+ind])))
-        H.append(PointSet(np.array(text[["xi", "yi", "zi"]][1+ind+ND:1+ND+NH+ind])))
+        ind = frame * (ND + NH)
+        D.append(PointSet(np.array(text[["xi", "yi", "zi"]][1 + ind:1 + ND + ind])))
+        H.append(PointSet(np.array(text[["xi", "yi", "zi"]][1 + ind + ND:1 + ND + NH + ind])))
     return D, H
-    
