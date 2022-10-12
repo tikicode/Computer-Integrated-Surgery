74b144dc78f39525e960a2beba68b2d62ed53060
tikicode <anukala1@jhu.edu>
2022-10-12 18:33:28 -0400

    finished file writing

---
+++ pa_one.py
@@ -0,0 +1,38 @@
+import pa_one.PROGRAMS.prob456 as prob
+import click
+from pathlib import Path
+
+
+@click.command()
+@click.option("--data-dir", "-d", default=r"..\data", help="Input data directory")
+@click.option("--output-dir", "-o", default=r"..\pa_one\OUTPUT", help="Output directory")
+@click.option("--name", "-n", default="pa1-debug-a", help="Name of file")
+@click.option()
+def main(data_dir, output_dir, name):
+    data_dir = Path(data_dir)
+    output_dir = Path(output_dir)
+
+    cal_body = data_dir / f"{name}-calbody.txt"
+    cal_reading = data_dir / f"{name}-calreadings.txt"
+    em_pivot = data_dir / f"{name}-empivot.txt"
+    opt_pivot = data_dir / f"{name}-optpivot.txt"
+
+    c_exp = prob.prob_four(cal_body, cal_reading)
+    probe = prob.prob_five(em_pivot)
+    beacon = prob.prob_six(opt_pivot, cal_body, cal_reading)
+
+
+def write_output(c_exp, probe, beacon, output_dir, name):
+    f = open(f"{output_dir}/{name}-our-sol", 'w')
+    f.write('{0} {1} {2}\n'.format(len(c_exp[0].points[0]), len(c_exp), name))
+    f.write('{0:.2f} {1:.2f} {2:.2f}\n'.format(probe[0], probe[1], probe[2]))
+    f.write('{0:.2f} {1:.2f} {2:.2f}\n'.format(beacon[0], beacon[1], beacon[2]))
+    for r in range(len(c_exp)):
+        for c in range(len(c_exp[0].points[1])):
+            f.write('{0:.2f} {1:.2f} {2:.2f}\n'.format(c_exp[r].points[0][c], c_exp[r].points[1][c],
+                                                       c_exp[r].points[2][c]))
+    f.close()
+
+
+if __name__ == "__main__":
+    main()
--- pa_one/PROGRAMS/prob456.py
+++ pa_one/PROGRAMS/prob456.py
@@ -7,8 +7,8 @@ import cis.io as io
 
 def prob_four(cal_body, cal_reading):
     # do something to read the files
-    # calbod = open(calbody, "r")
-    # calread = open(calReading, "r")
+    # cal_bod = open(cal_body, "r")
+    # cal_read = open(calReading, "r")
     # read file method
 
     cal_bod = io.getDataCalBody(cal_body)
@@ -18,25 +18,25 @@ def prob_four(cal_body, cal_reading):
     c_points = cal_bod[2]
     D_points = cal_read[0]
     A_points = cal_read[1]
-    cExp = []
+    c_exp = []
     for frame in range(len(D_points)):
         FD = ps.registration(d_points, D_points[frame])
         FA = ps.registration(a_points, A_points[frame])
         Ci = FA.compose_vec(c_points)
         Ci = (FD.invert()).compose_frame(Ci)
-        cExp.append(Ci)
+        c_exp.append(Ci)
 
-    return cExp
+    return c_exp
 
 
-def prob_five(filename):
-    em_data = io.getDataEMPivot(filename)
+def prob_five(em_pivot):
+    em_data = io.getDataEMPivot(em_pivot)
     pivot_em = pc.pivot(em_data, 0)
     return pivot_em[0][0:3]
 
 
-def prob_six(filename, cal_body, cal_reading):
-    opt = io.getDataOptPivot(filename)
+def prob_six(opt_pivot, cal_body, cal_reading):
+    opt = io.getDataOptPivot(opt_pivot)
     cal_bod = io.getDataCalBody(cal_body)
     cal_read = io.getDataCalReading(cal_reading)
     d_points = cal_bod[0]
